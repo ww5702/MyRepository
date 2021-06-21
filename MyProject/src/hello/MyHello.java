3facdb24d20b8179d0c0e76fb39ed647d53a60ba
Good <Good@DESKTOP-BAP0IVA>
2021-06-21 18:33:25 +0900

    push in git[dev]

--- MyProject/src/hello/MyHello.java
+++ MyProject/src/hello/MyHello.java
@@ -5,7 +5,7 @@ public class MyHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("This is 2nd version");
+		System.out.println("This is 2nd version!");
 	}
 
 }
